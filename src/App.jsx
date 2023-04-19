1f8cda200298ad76a3451513fc1c12022929c9d8
facindito <facinditto@gmail.com>
2023-04-18 22:12:05 -0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from './components/Header'
 import CartProvider from './context/cart'
 import { Outlet } from 'react-router-dom'
 
@@ -5,6 +6,7 @@ export default function App () {
   return (
     <div className='max-w-6xl mx-auto flex flex-col overflow-hidden'>
       <CartProvider>
+        <Header />
         <Outlet />
       </CartProvider>
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+import { CartIcon } from './icons'
+
+export default function Header () {
+  return (
+    <header className='flex justify-between items-center'>
+      <NavLink
+        to='/'
+        className='p-2 m-4 border border-slate-300 rounded-md hover:bg-slate-300'
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/cart'
+        className='p-2 m-4 border border-slate-300 rounded-md hover:bg-slate-300'
+      >
+        <CartIcon />
+      </NavLink>
+    </header>
+  )
+};
